f4240bfa7d35358de4f6959c91035652e15fbc49
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-05-11 23:09:11 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar';
 
 const number = 55555;
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
